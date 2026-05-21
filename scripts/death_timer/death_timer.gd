c28b5a8dc4a41aa7b30889a8cd1de6fd017c6386
CurtMatthewGarcia <pokeminecraftexpert@gmail.com>
2026-05-22 00:57:59 +0800

    feat: added timer, timeout, and ending early when door is opened

---
+++ scripts/death_timer/death_timer.gd
@@ -0,0 +1,12 @@
+extends Control
+
+signal timeout
+
+@onready var timer = $Background/TimerLabel/Timer
+
+func emit_timeout() -> void:
+	timeout.emit()
+
+func end() -> void:
+	timer.stop()
+	timeout.emit()
---
+++ scripts/death_timer/death_timer_label.gd
@@ -0,0 +1,5 @@
+extends RichTextLabel
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.text = "%07.5f" % $Timer.time_left
