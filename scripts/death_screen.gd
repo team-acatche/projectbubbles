8b657a4393a404cc5939b23796eb5a1a36858a37
CurtMatthewGarcia <pokeminecraftexpert@gmail.com>
2026-05-26 20:53:49 +0800

    feat: added death animation when door is opened / timer ends

---
+++ scripts/death_screen.gd
@@ -0,0 +1,10 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_on_viewport_size_changed()
+	get_viewport().size_changed.connect(_on_viewport_size_changed)
+
+func _on_viewport_size_changed():
+	size = get_viewport_rect().size
--- scripts/interactible.gd
+++ scripts/interactible.gd
@@ -1,17 +1,26 @@
 extends Node2D
+class_name Interactible
 
 @export var collider_shape: CollisionShape2D
 
+var mouse_is_in_collider: bool = false
+
 signal interacted_with
 
-func _input(event: InputEvent) -> void:
+func _process(_delta: float) -> void:
 	var shape = collider_shape.shape
-	var mouse_is_in_collider = shape.collide(
+	mouse_is_in_collider = shape.collide(
 		Transform2D(0.0, collider_shape.global_position),
 		shape,
 		Transform2D(0.0, get_global_mouse_position())
 	)
 	
+	if mouse_is_in_collider:
+		return Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	
+
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click") and mouse_is_in_collider:
 		interacted_with.emit()
 
